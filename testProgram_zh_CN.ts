4ed372e0af81b57d56e89daa0f1350124bc7f60a
LRY <renyuanli@zhlxkj.com>
2025-10-17 16:34:44 +0800

    under ubuntu20.04

--- testProgram_zh_CN.ts
+++ testProgram_zh_CN.ts
@@ -20,196 +20,196 @@
     <name>MainWindow</name>
     <message>
         <location filename="mainwindow.ui" line="14"/>
-        <location filename="build/testProgram_autogen/include/ui_mainwindow.h" line="471"/>
+        <location filename="build/testProgram_autogen/include/ui_mainwindow.h" line="466"/>
         <source>MainWindow</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="34"/>
-        <location filename="build/testProgram_autogen/include/ui_mainwindow.h" line="472"/>
+        <location filename="build/testProgram_autogen/include/ui_mainwindow.h" line="467"/>
         <source>测试</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="52"/>
-        <location filename="build/testProgram_autogen/include/ui_mainwindow.h" line="473"/>
+        <location filename="build/testProgram_autogen/include/ui_mainwindow.h" line="468"/>
         <source>设置项</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="70"/>
-        <location filename="build/testProgram_autogen/include/ui_mainwindow.h" line="474"/>
+        <location filename="build/testProgram_autogen/include/ui_mainwindow.h" line="469"/>
         <source>设备点表</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="88"/>
-        <location filename="build/testProgram_autogen/include/ui_mainwindow.h" line="475"/>
+        <location filename="build/testProgram_autogen/include/ui_mainwindow.h" line="470"/>
         <source>历史记录</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="125"/>
-        <location filename="build/testProgram_autogen/include/ui_mainwindow.h" line="476"/>
+        <location filename="build/testProgram_autogen/include/ui_mainwindow.h" line="471"/>
         <source>简要信息</source>
         <oldsource>主测试界面：简要信息</oldsource>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="149"/>
-        <location filename="build/testProgram_autogen/include/ui_mainwindow.h" line="477"/>
+        <location filename="build/testProgram_autogen/include/ui_mainwindow.h" line="472"/>
         <source>测试选项</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="157"/>
-        <location filename="build/testProgram_autogen/include/ui_mainwindow.h" line="478"/>
+        <location filename="build/testProgram_autogen/include/ui_mainwindow.h" line="473"/>
         <source>选择要闭合的继电器：</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="164"/>
         <location filename="mainwindow.ui" line="203"/>
+        <location filename="build/testProgram_autogen/include/ui_mainwindow.h" line="474"/>
         <location filename="build/testProgram_autogen/include/ui_mainwindow.h" line="479"/>
-        <location filename="build/testProgram_autogen/include/ui_mainwindow.h" line="484"/>
         <source>1#继电器</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="171"/>
         <location filename="mainwindow.ui" line="210"/>
+        <location filename="build/testProgram_autogen/include/ui_mainwindow.h" line="475"/>
         <location filename="build/testProgram_autogen/include/ui_mainwindow.h" line="480"/>
-        <location filename="build/testProgram_autogen/include/ui_mainwindow.h" line="485"/>
         <source>2#继电器</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="178"/>
         <location filename="mainwindow.ui" line="217"/>
+        <location filename="build/testProgram_autogen/include/ui_mainwindow.h" line="476"/>
         <location filename="build/testProgram_autogen/include/ui_mainwindow.h" line="481"/>
-        <location filename="build/testProgram_autogen/include/ui_mainwindow.h" line="486"/>
         <source>3#继电器</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="185"/>
         <location filename="mainwindow.ui" line="224"/>
+        <location filename="build/testProgram_autogen/include/ui_mainwindow.h" line="477"/>
         <location filename="build/testProgram_autogen/include/ui_mainwindow.h" line="482"/>
-        <location filename="build/testProgram_autogen/include/ui_mainwindow.h" line="487"/>
         <source>4#继电器</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="196"/>
-        <location filename="build/testProgram_autogen/include/ui_mainwindow.h" line="483"/>
+        <location filename="build/testProgram_autogen/include/ui_mainwindow.h" line="478"/>
         <source>选择要测试的继电器：</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="233"/>
-        <location filename="build/testProgram_autogen/include/ui_mainwindow.h" line="488"/>
+        <location filename="build/testProgram_autogen/include/ui_mainwindow.h" line="483"/>
         <source>开始测试</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="240"/>
-        <location filename="build/testProgram_autogen/include/ui_mainwindow.h" line="489"/>
+        <location filename="build/testProgram_autogen/include/ui_mainwindow.h" line="484"/>
         <source>停止测试</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="258"/>
-        <location filename="build/testProgram_autogen/include/ui_mainwindow.h" line="490"/>
+        <location filename="build/testProgram_autogen/include/ui_mainwindow.h" line="485"/>
         <source>设备状态</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="271"/>
-        <location filename="build/testProgram_autogen/include/ui_mainwindow.h" line="491"/>
+        <location filename="build/testProgram_autogen/include/ui_mainwindow.h" line="486"/>
         <source>测试计数</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="305"/>
-        <location filename="build/testProgram_autogen/include/ui_mainwindow.h" line="492"/>
+        <location filename="mainwindow.ui" line="289"/>
+        <location filename="build/testProgram_autogen/include/ui_mainwindow.h" line="487"/>
         <source>发送测试命令</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="318"/>
-        <location filename="build/testProgram_autogen/include/ui_mainwindow.h" line="493"/>
+        <location filename="mainwindow.ui" line="302"/>
+        <location filename="build/testProgram_autogen/include/ui_mainwindow.h" line="488"/>
         <source>ioa1开</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="331"/>
-        <location filename="build/testProgram_autogen/include/ui_mainwindow.h" line="494"/>
+        <location filename="mainwindow.ui" line="315"/>
+        <location filename="build/testProgram_autogen/include/ui_mainwindow.h" line="489"/>
         <source>ioa1关</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="349"/>
-        <location filename="build/testProgram_autogen/include/ui_mainwindow.h" line="495"/>
+        <location filename="mainwindow.ui" line="333"/>
+        <location filename="build/testProgram_autogen/include/ui_mainwindow.h" line="490"/>
         <source>远端从站端口：</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="443"/>
-        <location filename="build/testProgram_autogen/include/ui_mainwindow.h" line="496"/>
+        <location filename="mainwindow.ui" line="427"/>
+        <location filename="build/testProgram_autogen/include/ui_mainwindow.h" line="491"/>
         <source>本地主站端口：</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="455"/>
-        <location filename="build/testProgram_autogen/include/ui_mainwindow.h" line="497"/>
+        <location filename="mainwindow.ui" line="439"/>
+        <location filename="build/testProgram_autogen/include/ui_mainwindow.h" line="492"/>
         <source>远端从站地址：</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="509"/>
-        <location filename="build/testProgram_autogen/include/ui_mainwindow.h" line="498"/>
+        <location filename="mainwindow.ui" line="493"/>
+        <location filename="build/testProgram_autogen/include/ui_mainwindow.h" line="493"/>
         <source>连接</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="527"/>
-        <location filename="build/testProgram_autogen/include/ui_mainwindow.h" line="499"/>
+        <location filename="mainwindow.ui" line="511"/>
+        <location filename="build/testProgram_autogen/include/ui_mainwindow.h" line="494"/>
         <source>断开连接</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="545"/>
-        <location filename="build/testProgram_autogen/include/ui_mainwindow.h" line="500"/>
+        <location filename="mainwindow.ui" line="529"/>
+        <location filename="build/testProgram_autogen/include/ui_mainwindow.h" line="495"/>
         <source>新增配置</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="563"/>
-        <location filename="build/testProgram_autogen/include/ui_mainwindow.h" line="501"/>
+        <location filename="mainwindow.ui" line="547"/>
+        <location filename="build/testProgram_autogen/include/ui_mainwindow.h" line="496"/>
         <source>保存配置</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="581"/>
-        <location filename="build/testProgram_autogen/include/ui_mainwindow.h" line="502"/>
+        <location filename="mainwindow.ui" line="565"/>
+        <location filename="build/testProgram_autogen/include/ui_mainwindow.h" line="497"/>
         <source>删除配置</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="595"/>
-        <location filename="build/testProgram_autogen/include/ui_mainwindow.h" line="503"/>
+        <location filename="mainwindow.ui" line="579"/>
+        <location filename="build/testProgram_autogen/include/ui_mainwindow.h" line="498"/>
         <source>本地主站地址：</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="607"/>
-        <location filename="build/testProgram_autogen/include/ui_mainwindow.h" line="504"/>
+        <location filename="mainwindow.ui" line="591"/>
+        <location filename="build/testProgram_autogen/include/ui_mainwindow.h" line="499"/>
         <source>配置名称：</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="629"/>
-        <location filename="build/testProgram_autogen/include/ui_mainwindow.h" line="505"/>
+        <location filename="mainwindow.ui" line="613"/>
+        <location filename="build/testProgram_autogen/include/ui_mainwindow.h" line="500"/>
         <source>测试记录....</source>
         <translation type="unfinished"></translation>
     </message>
